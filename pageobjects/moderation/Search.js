5087f42f3178778dc6b308d30ef6952ba90dbe74
ManishTaggbox <77089863+ManishTaggbox@users.noreply.github.com>
2026-04-14 14:14:20 +0530

    Moderation Fix

--- pageobjects/moderation/Search.js
+++ pageobjects/moderation/Search.js
@@ -14,7 +14,7 @@ class Search
         this.searchBox = page.locator('//input[@placeholder="Search"]');
         this.searchCross = page.locator('//button[@aria-label="search"]');
         this.crossIcon = page.locator('.fa-xmark');
-        this.dropdownItem = page.locator('//a[text()="Tag A"]');
+        this.dropdownItem = page.locator("(//a[normalize-space()='TagA'])[1]");
     }
 
     async Search() 
